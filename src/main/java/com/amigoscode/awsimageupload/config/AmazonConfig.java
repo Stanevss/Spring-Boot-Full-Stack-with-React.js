5f7568701fde27100615b81a2036fb0dfcb15942
Stanevss <stanev.ss@gmail.com>
2022-03-20 20:10:51 +0100

    API & Service Layer Implementation

---
+++ src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,26 @@
+package com.amigoscode.awsimageupload.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                "AKIA2X7WYPB37OVSQHAN",
+                "y0WfIo4nL0IYyOc+W2jFUyKXPZBfoeArR+P1yUl0"
+        );
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withRegion("eu-central-1")
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
