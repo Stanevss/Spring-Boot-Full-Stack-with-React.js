4ed700a94a13b43b98f7461a7b4da5c6b8516f27
Stanevss <stanev.ss@gmail.com>
2022-04-09 22:49:58 +0200

    React Dropzone

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import axios from "axios";
+import { useDropzone } from "react-dropzone";
 
 const UserProfiles = () => {
   const [UserProfiles, setUserProfiles] = useState([]);
@@ -17,13 +18,36 @@ const UserProfiles = () => {
   }, []);
 
   return UserProfiles.map((userProfile, index) => {
-    return (<div key={index}>
+    return (
+      <div key={index}>
+        <br />
         <h1>{userProfile.username}</h1>
         <p>{userProfile.userProfileId}</p>
-    </div>
-    ) 
+        <Dropzone />
+        <br />
+      </div>
+    );
   });
 };
+
+function Dropzone() {
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop profile image, or click to select profile image</p>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
