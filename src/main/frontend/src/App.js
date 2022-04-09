b718d19d4b25728ec6285eb4b2d2048f82ec5cc7
Stanevss <stanev.ss@gmail.com>
2022-04-09 22:30:45 +0200

    Rendering User Profile

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+const UserProfiles = () => {
+  const [UserProfiles, setUserProfiles] = useState([]);
+
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/user-profile").then((res) => {
+      console.log(res);
+      setUserProfiles(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+
+  return UserProfiles.map((userProfile, index) => {
+    return (<div key={index}>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+    </div>
+    ) 
+  });
+};
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfiles />
+    </div>
+  );
+}
+
+export default App;
